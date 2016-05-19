2e6fb46537093c299d7fcd21fe2b9c6233a54735
Cameron Gose <cgose@bskinc.com>
2016-05-19 08:25:00 -0700

    Added recycler view and forecast list adapter class.

--- app/src/main/java/com/example/cgose/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/cgose/weatherapp/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.cgose.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-      message.text = "Hello Kotlin!"
-    }
+  private val items = listOf(
+    "Mon 6/23 - Sunny - 31/17",
+    "Tue 6/24 - Foggy - 31/17",
+    "Wed 6/25 - Cloudy - 31/17",
+    "Thu 6/26 - Rainy - 31/17",
+    "Fri 6/27 - Foggy - 31/17",
+    "Sat 6/28 - TRAPPED IN WEATHER STATION - 31/17",
+    "Sun 6/29 - Sunny - 31/17"
+  )
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+    forecastList.layoutManager = LinearLayoutManager(this)
+    forecastList.adapter = ForecastListAdapter(items)
+  }
 }
